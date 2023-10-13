84f0c4524903d8ea5daca2b8e88d20c75edb95bc
Ilyes Attila <attis.ilyes@gmail.com>
2023-10-13 16:39:13 +0300

    v1.3

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ let spaces = [];
 let foundedCharacter = 0;
 
 function startGame() {
-    word = document.getElementById("Word").value;
-    document.getElementById('Lifes-left').innerText = "Lifes left: " + life;
+    word = document.getElementById("word").value;
+    document.getElementById('lifes-left').innerText = "Lifes left: " + life;
     for (let charact = 0; charact < word.length; ++charact) {    
         spaces[charact] = "_";
     }
-    return (document.getElementById('FoundChar').innerText = spaces);
+    return (document.getElementById('foundChar').innerText = spaces);
 }
 
 function searchChar() {
     let found = false;
-    let searchCharacter = document.getElementById("Character").value;
+    let searchCharacter = document.getElementById("character").value;
     for (let i = 0; i < word.length; ++i) {
         if (word[i] == searchCharacter) {
             spaces[i] = searchCharacter;
@@ -26,19 +26,19 @@ function searchChar() {
         --life;
     }
     if (life == 0) {
-        document.getElementById('FinalRezult').innerText = "You lose";
+        document.getElementById('finalRezult').innerText = "You lose";
     }
     if (foundedCharacter == word.length) {
-        document.getElementById('FinalRezult').innerText = "You win";
+        document.getElementById('finalRezult').innerText = "You win";
     }
-    document.getElementById('Lifes-left').innerText = "Lifes left: " + life;
-    document.getElementById('FoundChar').innerText = spaces;
+    document.getElementById('lifes-left').innerText = "Lifes left: " + life;
+    document.getElementById('foundChar').innerText = spaces;
     return (clearInput());
 }
 
 function clearInput(){
-    var getValue = document.getElementById("Character");
-      if (getValue.value !="") {
-          getValue.value = "";
-      }
+    let getValue = document.getElementById("character");
+    if (getValue.value !="") {
+        getValue.value = "";
+    }
 }
